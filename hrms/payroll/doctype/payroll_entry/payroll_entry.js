4b5cf707a19366d3dd6423d957ecb84c87edee59
tw <tw@gmail.com>
2025-11-28 14:40:28 +0600

    payroll entry

--- hrms/payroll/doctype/payroll_entry/payroll_entry.js
+++ hrms/payroll/doctype/payroll_entry/payroll_entry.js
@@ -121,7 +121,7 @@ frappe.ui.form.on("Payroll Entry", {
 	create_salary_slips: function (frm) {
 		frm.call({
 			doc: frm.doc,
-			method: "run_doc_method",
+			// method: "run_doc_method",
 			args: {
 				method: "create_salary_slips",
 				dt: "Payroll Entry",
