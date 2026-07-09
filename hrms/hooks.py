30da3e3e095e65c1baabdc025217b975103a189b
pn <pn@gmail.com>
2026-07-09 09:16:14 +0600

    LA

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -132,9 +132,10 @@ before_app_uninstall = "hrms.setup.before_app_uninstall"
 # -----------
 # Permissions evaluated in scripted ways
 
-# permission_query_conditions = {
-# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
-# }
+permission_query_conditions = {
+	# "Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+    "Leave Application": "hrms.hr.doctype.leave_application.leave_application.get_permission_query_conditions",
+}
 #
 # has_permission = {
 # 	"Event": "frappe.desk.doctype.event.event.has_permission",
@@ -214,16 +215,23 @@ doc_events = {
 # ---------------
 
 scheduler_events = {
+	"cron": {
+        "* * * * *": [
+				"hrms.hr.doctype.shift_type.shift_type.update_last_sync_of_checkin",
+				"hrms.hr.doctype.shift_type.shift_type.process_auto_attendance_for_all_shifts",
+				"hrms.hr.doctype.shift_assignment_schedule.shift_assignment_schedule.process_auto_shift_creation",
+        ]
+    },
 	"all": [
 		"hrms.hr.doctype.interview.interview.send_interview_reminder",
 	],
 	"hourly": [
 		"hrms.hr.doctype.daily_work_summary_group.daily_work_summary_group.trigger_emails",
 	],
-	"hourly_long": [
-		"hrms.hr.doctype.shift_type.shift_type.process_auto_attendance_for_all_shifts",
-		"hrms.hr.doctype.shift_assignment_schedule.shift_assignment_schedule.process_auto_shift_creation",
-	],
+	# "hourly_long": [
+	# 	# "hrms.hr.doctype.shift_type.shift_type.process_auto_attendance_for_all_shifts",
+	# 	"hrms.hr.doctype.shift_assignment_schedule.shift_assignment_schedule.process_auto_shift_creation",
+	# ],
 	"daily": [
 		"hrms.controllers.employee_reminders.send_birthday_reminders",
 		"hrms.controllers.employee_reminders.send_work_anniversary_reminders",
